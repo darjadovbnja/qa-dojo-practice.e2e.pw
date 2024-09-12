6291bc36de919b8229675bbff975a2aaf73cc124
darjadovbnja <darja.dovbnja@gamil.com>
2024-09-12 21:48:54 +0300

    carousel_refactored

--- tests/telemart-tests/carousel_initial.spec.ts
+++ tests/telemart-tests/carousel_initial.spec.ts
@@ -1,36 +1,46 @@
-// - перевірити що в каруселі знаходиться більше 2х елементів (використовуйте locator().all() і length) 
-// - натиснути 2 рази на scroll каруселі 
-// - клікнути на контект каруселі 
-// - перевірити що навігація успішна  (подумайте як) 
-
-//проверить что в слайдере больше 2-х элементов 
-//2 раза кликнуть вперед
-//проверить uRL текущего баннера
-//кликнуть по нему
-//проверить что результирующий url = url баннера
-
-
-
 import {expect, Locator, test} from '@playwright/test';
+
+async function verifyNumberOfBanners(carouselBanner: Locator) {
+    const banners = await carouselBanner.all();
+    await expect(banners.length).toBeGreaterThan(2); 
+};
+
+async function clickButtonForward(buttonForward: Locator, timesClick: number) {
+    for (let i=0; i<timesClick; i++) {
+        await buttonForward.click();
+    }
+};
+
+async function getActiveBannerUrl(carouselActiveBanner: Locator) {
+    return await carouselActiveBanner.getAttribute('href');//class="swiper-slide"
+}
 
 test.beforeEach(async( {page} ) => {
     await page.goto('https://telemart.ua/ua/');
-})
+});
 
-test('active banner navigation', async( {page} ) => {
+test.afterEach(({ page }) => {
+    page.close();
+  });
+
+test('active banner navigation', async({page}) => {
     const carousel: Locator = page.locator('//*[@class="categories-slider"]');
     const carouselBanner: Locator = page.locator('//*[@class="categories-slider"]//*[@class="categories-slider__image"]');
     const carouselActiveBanner: Locator = page.locator('//*[@class="categories-slider"]//*[@class="swiper-wrapper"]/*[@class="swiper-slide swiper-slide-active"]');
-
-    const carouselScroll: Locator = page.locator("//*[contains(@class, 'categories-slider-progress-bar-container__progress-bar')]");
     const buttonForward: Locator = page.locator('//*[@class="swiper-button-next"]')
 
-    await expect((await carouselBanner.all()).length).toBeGreaterThan(2);
-    await buttonForward.click();
-    await buttonForward.click();
-    const ulrOfActiveBanner = await carouselActiveBanner.getAttribute('href')
+   //await expect((await carouselBanner.all()).length).toBeGreaterThan(2);
+   await verifyNumberOfBanners(carouselBanner);
+
+    // await buttonForward.click();
+    // await buttonForward.click();
+    await clickButtonForward(buttonForward, 3);
+
+    //const ulrOfActiveBanner = await carouselActiveBanner.getAttribute('href')
+    const ulrOfActiveBanner = await getActiveBannerUrl(carouselActiveBanner);
 
     await carouselActiveBanner.click()
-
+    
+    //await expect(page.url()).toEqual(ulrOfActiveBanner);
     await expect(page.url()).toEqual(ulrOfActiveBanner);
-})
+});
