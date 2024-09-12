e25c4d4e4ab2d882b7c76e0f0bf8c9ed52498913
darjadovbnja <darja.dovbnja@gamil.com>
2024-09-12 23:52:41 +0300

    carousel_refactored fix

---
+++ tests/telemart-tests/carousel_refactored.spec.ts
@@ -0,0 +1,50 @@
+import {expect, Locator, test} from '@playwright/test';
+
+async function verifyNumberOfBanners(carouselBanner: Locator) {
+    const banners = await carouselBanner.all();
+    await expect(banners.length).toBeGreaterThan(2); 
+};
+
+async function clickButtonForward(buttonForward: Locator, timesClick: number) {
+    for (let i=0; i<timesClick; i++) {
+        await buttonForward.click();
+    }
+};
+
+async function getActiveBannerUrl(carouselActiveBanner: Locator) {
+    return await carouselActiveBanner.getAttribute('href');//class="swiper-slide"
+}
+
+function removeUaFromUrl(url: string): string {
+    return url.replace("/ua/", "/");
+  }
+
+test.beforeEach(async( {page} ) => {
+    await page.goto('https://telemart.ua/ua/');
+});
+
+test.afterEach(({ page }) => {
+    page.close();
+  });
+
+test('active banner navigation', async({page}) => {
+    const carousel: Locator = page.locator('//*[@class="categories-slider"]');
+    const carouselBanner: Locator = page.locator('//*[@class="categories-slider"]//*[@class="categories-slider__image"]');
+    const carouselActiveBanner: Locator = page.locator('//*[@class="categories-slider"]//*[@class="swiper-wrapper"]/*[@class="swiper-slide swiper-slide-active"]');
+    const buttonForward: Locator = page.locator('//*[@class="swiper-button-next"]')
+
+   //await expect((await carouselBanner.all()).length).toBeGreaterThan(2);
+   await verifyNumberOfBanners(carouselBanner);
+
+    // await buttonForward.click();
+    // await buttonForward.click();
+    await clickButtonForward(buttonForward, 3);
+
+    //const ulrOfActiveBanner = await carouselActiveBanner.getAttribute('href')
+    const urlOfActiveBanner = await getActiveBannerUrl(carouselActiveBanner);
+    console.log(urlOfActiveBanner);
+        
+    await carouselActiveBanner.click()
+    
+    await expect(removeUaFromUrl(page.url())).toEqual(urlOfActiveBanner);
+});
