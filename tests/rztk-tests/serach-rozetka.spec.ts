b91076cbb751ebd4630ccd49f935ddc146ae3232
darjadovbnja <darja.dovbnja@gamil.com>
2024-10-14 22:40:52 +0300

    testData for rztk

--- tests/rztk-tests/serach-rozetka.spec.ts
+++ tests/rztk-tests/serach-rozetka.spec.ts
@@ -1,12 +1,31 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test('search for product', async({page}) => {
-    await page.goto('https://rozetka.com.ua/');
-    await page.locator("input[name='search']").fill('monitor');
-    await page.locator("input[name='search']").press('Enter');
+const arr = []; // for of
+const object = {}; // for in
 
-    console.log('test');
+const testData = [
+    {
+        testId: 'rztk-001',
+        searchQuery: 'Монитор',
+        searchResultWord: 'Монитор',
+    }, 
+    {
+        testId: 'rztk-002',
+        searchQuery: 'Карта',
+        searchResultWord: 'Карта',
+    },
+]
+
+ 
+for (const data of testData) {
+    test(`${data.testId}. Search for ${data.searchQuery}`, async({page}) => {
+        await page.goto('https://rozetka.com.ua/');
+        await page.locator("input[name='search']").fill(data.searchQuery);
+        await page.locator("input[name='search']").press('Enter');
     
-
+        const searchResult = page.locator(`//div[contains(@class, 'goods-tile__inner')]//a//img`).first();
     
-})
+        expect(searchResult.getAttribute('title')).toContain(data.searchResultWord);
+            
+    })
+}
